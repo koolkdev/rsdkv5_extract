b39d7741e2bb889278a9abe9289c174f52aebeb5
koolkdev <koolkdev@gmail.com>
2017-09-06 22:12:13 +0300

    mdl2stl: add comments, rename things

--- mdl2stl.py
+++ mdl2stl.py
@@ -6,14 +6,15 @@ MDL = Struct(
     # Flags
     Embedded(FlagsEnum(Int8ul,
         HasNormals=1,
-        MaybeHasTextureCoordinates=2,
+        HasUnknown=2,
         HasColors=4)),
-    "FaceEdges" / Int8ul,  # 3 or 4
-    "VerticiesCount" / Int16ul,
+    # How many vertices in face. 3 or 4
+    "FaceVerticesCount" / Int8ul,
+    "VerticesCount" / Int16ul,
     "FramesCount" / Int16ul,
 
-    If(lambda ctx: ctx.MaybeHasTextureCoordinates,
-        "MaybeTextureCoordinates" / Array(this.VerticiesCount,
+    If(lambda ctx: ctx.HasUnknown,
+        "UnknownList" / Array(this.VerticesCount,
             Struct(
                 "x" / Float32l,
                 "y" / Float32l,
@@ -22,7 +23,7 @@ MDL = Struct(
     ),
 
     If(lambda ctx: ctx.HasColors,
-        "Colors" / Array(this.VerticiesCount,
+        "Colors" / Array(this.VerticesCount,
             Struct(
                 "r" / Int8ul,
                 "g" / Int8ul,
@@ -32,20 +33,18 @@ MDL = Struct(
         )
     ),
 
-    "FacesEdgesCount" / Int16ul,
-    "Faces" / Array(this.FacesEdgesCount / this.FaceEdges,
-        Struct(
-            "Verticies" / Array(this._.FaceEdges, Int16ul)
-        )
-    ),
+    # Should be in multiply of FaceVerticesCount
+    # It has FaceVerticesCount vertices for each face
+    "FacesVertices" / PrefixedArray(Int16ul, Int16ul),
 
     "Frames" / Array(this.FramesCount,
         Struct(
-            "Verticies" / Array(this._.VerticiesCount,
+            "Vertices" / Array(this._.VerticesCount,
                 Struct(
                     "x" / Float32l,
                     "y" / Float32l,
                     "z" / Float32l,
+                    # I am not sure if it is normals
                     If(this._._.HasNormals,
                         "Normal" / Struct(
                             "x" / Float32l,
@@ -72,7 +71,7 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     mdl = MDL.parse(open(sys.argv[1], "rb").read())
-    if not 3 <= mdl.FaceEdges <= 4:
+    if not 3 <= mdl.FaceVerticesCount <= 4:
         print "Unsupported model"
         sys.exit(-1)
     # TODO: Colors (need to be binary stl)
@@ -86,21 +85,23 @@ if __name__ == "__main__":
         else:
             output = open(base_name + ".stl", "w")
         output.write("solid obj\n")
-        for face in mdl.Faces:
+        for i in xrange(0, len(mdl.FacesVertices), mdl.FaceVerticesCount):
+            face_vertices = [mesh.Vertices[x] for x in mdl.FacesVertices[i:i+mdl.FaceVerticesCount]]
             # TODO: normal per vertex (need other file format?)
             output.write("  facet normal 0 0 0\n")
             output.write("    outer loop\n")
-            write_vertex(mesh.Verticies[face.Verticies[0]])
-            write_vertex(mesh.Verticies[face.Verticies[1]])
-            write_vertex(mesh.Verticies[face.Verticies[2]])
+            write_vertex(face_vertices[0])
+            write_vertex(face_vertices[1])
+            write_vertex(face_vertices[2])
             output.write("    endloop\n")
             output.write("  endfacet\n")
-            if mdl.FaceEdges == 4:
+            if mdl.FaceVerticesCount == 4:
+                # If it is quad-face, split it to two triangle faces
                 output.write("  facet normal 0 0 0\n")
                 output.write("    outer loop\n")
-                write_vertex(mesh.Verticies[face.Verticies[0]])
-                write_vertex(mesh.Verticies[face.Verticies[2]])
-                write_vertex(mesh.Verticies[face.Verticies[3]])
+                write_vertex(face_vertices[0])
+                write_vertex(face_vertices[2])
+                write_vertex(face_vertices[3])
                 output.write("    endloop\n")
                 output.write("  endfacet\n")
         output.write("endsolid\n")
