31f912df35470cc7decf47a4e0604f81a6702219
koolkdev <koolkdev@gmail.com>
2017-09-09 00:25:33 +0300

    Update scene struct

--- parse_scene.py
+++ parse_scene.py
@@ -1,7 +1,7 @@
 from construct import *
 import sys
 
-String = PascalString(Byte)
+LString = PascalString(Byte)
 Word = Int16ul
 Dword = Int32ul
 
@@ -12,22 +12,27 @@ def CompressedZlib(subcon):
         "compressed_data" / Compressed(subcon, "zlib"),
     ))
 
+def inc(ctx, var):
+    res = ctx[var]
+    ctx[var] += 1
+    return res
+
 SCN = Struct(
     "magic" / Const("SCN\0"),
 
     # Ignored
     "skipped_header" / Bytes(16),
     # Name of some bin file
-    "skipped_bin_name" / String,
+    "skipped_bin_name" / LString,
     "skipped_byte" / Byte,
 
     "Views" / PrefixedArray(Byte, Struct(
         # This is read as part as the length of next string, and ignored
         "ignored_byte" / Byte,
-        "Name" / String,
+        "Name" / LString,
 
         "unknown_byte_1" / Byte,
-        "unknown_byte_2" / Byte,
+        "unknown_byte_2" / Byte,  # Related to background switch?
 
         "Width" / Word,
         "Height" / Word,
@@ -35,22 +40,63 @@ SCN = Struct(
         "unknown_word_5" / Word,
         "unknown_word_6" / Word,
 
-        "some_array" / PrefixedArray(Word, Struct(
+        "ScrollArray" / PrefixedArray(Word, Struct(
             "unknown_word_1" / Word,
             "unknown_word_2" / Word,
             "unknown_byte_1" / Byte,
             "unknown_byte_2" / Byte,
         )),
 
-        "some_array" / CompressedZlib(Bytes(this.Height * 0x10)),
+        "ScrollingInfo" / CompressedZlib(Bytes(this.Height * 0x10)),
 
         "Tiles" / CompressedZlib(Array(this.Width * this.Height, Word))
     )),
 
-    "bytecode_things" / PrefixedArray(Byte, Struct(
-        # TODO
-    ))
+    "ObjectsInstances" / PrefixedArray(Byte, Struct(
+        "ObjectNameHash" / Bytes(16),
 
+        "AttributesCount" / Byte,
+        "AttributesInfo" / Array(this.AttributesCount-1, Struct(
+            "AttributeNameHash" / Bytes(16),
+            "AttributeType" / Enum(Byte,
+                UINT8=0,
+                UINT16=1,
+                UINT32=2,
+                INT8=3,
+                INT16=4,
+                INT32=5,
+                DWORD=6,
+                BOOL=7,
+                STRING=8,
+                VECTOR2D=9,
+                COLOR=11,
+            )
+        )),
+
+        "InstancesAttributes" / PrefixedArray(Word, Struct(
+            "ObjectID" / Word,
+            "X" / Dword,
+            "Y" / Dword,
+            "i" / Computed(0),
+            "Attributes" / Array(this._.AttributesCount-1,
+                Switch(lambda this: this._.AttributesInfo[inc(this, "i")].AttributeType,
+                    {
+                        "UINT8": Byte,
+                        "UINT16": Word,
+                        "UINT32": Dword,
+                        "INT8": Int8sl,
+                        "INT16": Int16sl,
+                        "INT32": Int32sl,
+                        "DWORD": Dword,
+                        "BOOL": Dword,
+                        "STRING": FocusedSeq(1, "length" / Word, "value" / String(this.length * 2, "utf16", "\xff")),
+                        "VECTOR2D": Struct("X" / Dword, "Y" / Dword),
+                        "COLOR": Struct("R" / Byte, "G" / Byte, "B" / Byte, "A" / Byte),
+                    }
+                )
+            )
+        ))
+    ))
 )
 
 if __name__ == "__main__":
