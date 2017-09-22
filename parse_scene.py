df2fdcb8ddb9927ee1801517b33a90060808567a
koolkdev <koolkdev@gmail.com>
2017-09-22 21:47:05 +0300

    parse_scene.py: fix scrolling info size (fix #6)

--- parse_scene.py
+++ parse_scene.py
@@ -44,7 +44,7 @@ SCN = Struct(
         "ignored_byte" / Byte,
         "Name" / LString,
 
-        "unknown_byte_1" / Byte,
+        "IsScrollingVertical" / Byte,
         "unknown_byte_2" / Byte,  # Related to background switch?
 
         "Width" / Word,
@@ -60,7 +60,7 @@ SCN = Struct(
             "unknown_byte_2" / Byte,
         )),
 
-        "ScrollingInfo" / CompressedZlib(Bytes(this.Height * 0x10)),
+        "ScrollingInfo" / CompressedZlib(Bytes(lambda this: (this.IsScrollingVertical and this.Width or this.Height) * 0x10)),
 
         "Tiles" / CompressedZlib(Array(this.Width * this.Height, Word))
     )),
